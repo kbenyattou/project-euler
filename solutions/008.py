042fbb86e3c034b923431a10f5b60f51cd962228
Khallil Benyattou <61755728+kbenyattou@users.noreply.github.com>
2020-10-25 23:01:05 +0000

    Add files via upload

---
+++ solutions/008.py
@@ -0,0 +1,65 @@
+numberstring = '''
+73167176531330624919225119674426574742355349194934
+96983520312774506326239578318016984801869478851843
+85861560789112949495459501737958331952853208805511
+12540698747158523863050715693290963295227443043557
+66896648950445244523161731856403098711121722383113
+62229893423380308135336276614282806444486645238749
+30358907296290491560440772390713810515859307960866
+70172427121883998797908792274921901699720888093776
+65727333001053367881220235421809751254540594752243
+52584907711670556013604839586446706324415722155397
+53697817977846174064955149290862569321978468622482
+83972241375657056057490261407972968652414535100474
+82166370484403199890008895243450658541227588666881
+16427171479924442928230863465674813919123162824586
+17866458359124566529476545682848912883142607690042
+24219022671055626321111109370544217506941658960408
+07198403850962455444362981230987879927244284909188
+84580156166097919133875499200524063689912560717606
+05886116467109405077541002256983155200055935729725
+71636269561882670428252483600823257530420752963450'''
+
+numberint = int(numberstring.replace('\n', ''))
+
+# This function takes a string of integers and converts it into a list of its
+# constituent integers e.g. '1234' converts to [1,2,3,4].
+
+def convertlist(x):
+    x = list(x)
+    for j in range(0,len(x)):
+        x[j] = int(x[j])
+    return x
+
+# This function will take a string of integers and find the product of its
+# consitutent digits.
+
+def prod(x):
+    x = convertlist(x)
+    product = 1
+    for i in x:
+        product = product * i
+    return product
+
+# We'll create a set of all the possible adjacent 13-digit strings from the
+# given 1000-digit number.
+
+digitstrings = []
+for i in range(0,1001-13):
+    digitstrings.append(str(numberint)[i:i+13])
+
+# Given a list of integer strings, prodmax() returns the string with the
+# greatest product of its constituent digits.
+
+def prodmax(x):
+    term = x[0]
+    i = 0
+    while i < len(x):
+        if prod(term) < prod(x[i]):
+            term = x[i]
+        i += 1
+    return convertlist(term)
+
+print(f'''{numberstring}\n
+The thirteen adjacent digits with the greatest product in the number above are
+{prodmax(digitstrings)} and their product is {prod(prodmax(digitstrings))}.''')
