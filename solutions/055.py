7d50bbfde2e1c6531b736bef7bd47628e7265026
Khallil Benyattou <61755728+kbenyattou@users.noreply.github.com>
2020-11-04 00:17:50 +0000

    Add files via upload

---
+++ solutions/055.py
@@ -0,0 +1,21 @@
+def reverse_and_add(x):
+    return x + int(str(x)[::-1])
+
+def palindrome_check(y):
+    if str(y) == (str(y))[::-1]:
+        return True
+    return False
+
+total_lychrels = 0
+for i in range(1,10000):
+    n = 1
+    while n < 50:
+        i = reverse_and_add(i)
+        if palindrome_check(i):
+            total_lychrels += 1
+            break
+        else:
+            pass
+        n += 1
+
+print(f'''There are {9999-total_lychrels} Lychrel numbers less than 10000.''')
